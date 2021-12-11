719b7551d709e02d3e3f5db09db9d7cdd44c50b1
Tom Nicholson <tfwnicholson+1@gmail.com>
2021-12-11 12:16:03 +0000

    Map and reduce over trees

--- sketches/trees.js
+++ sketches/trees.js
@@ -1,5 +1,6 @@
 const canvasSketch = require("canvas-sketch");
 import {random} from "canvas-sketch-util"
+var _ = require("lodash");
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -15,7 +16,6 @@ class Tree {
   }
 
   draw(context) {
-    console.log(this.pos);
     context.lineTo(...this.pos);
     context.stroke();
     this.children.forEach(element => {
@@ -26,7 +26,44 @@ class Tree {
   }
 }
 
-const trees = new Tree([500, 500], [new Tree([400, 600]), new Tree([600, 600])]);
+function map(func, tree) {
+    return new Tree(func(tree.pos), _.map(tree.children, _.partial(map, func)));
+}
+
+function reduce(funcAcross, funcAlong, accum, tree) {
+  if(!tree.children || tree.children.length === 0) {
+    return funcAlong(accum, tree.pos);
+  } else {
+    const mapFunc = _.partial(reduce, funcAcross, funcAlong, accum);
+    console.log(tree.children.map(mapFunc));
+    return funcAlong(tree.pos, funcAcross(tree.children.map(mapFunc)));
+  }
+  console.log("Here?");
+}
+
+function depth(tree) {
+  const ones = (pos) => 1;
+  const plus = (a, b) => a + b;
+  console.log(map(ones, tree));
+  return reduce(_.max, plus, 0, map(ones, tree));
+}
+
+class TreeGenerator {
+  constructor(startPos, depth, tree) {
+    this.startPos = startPos;
+    this.depth = depth;
+    this.tree = tree;
+  }
+
+  next() {
+    if(this.tree === undefined) {
+      this.tree = new Tree(this.startPos);
+      return this.tree;
+    }
+  }
+}
+
+const tree = new Tree([500, 500], [new Tree([400, 600], [new Tree([800, 900])]), new Tree([600, 600])]);
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -38,7 +75,8 @@ const sketch = () => {
     context.beginPath();
     context.moveTo(100, 100);
     context.lineTo(200, 200);
-    trees.draw(context);
+    tree.draw(context);
+    console.log(depth(tree));
   };
 };
 
